a9df623b5bc07b7d2c619a058f4412c8c50410be
isaacophoven <824067@mystma.org>
2021-09-14 14:07:56 -0500

    ok

--- MKain.py
+++ MKain.py
@@ -1,1 +1,20 @@
-#rtrt
+def topFiveMovies():
+    #print the first movie
+    print("Lord of the Rings")
+
+    #Second movie
+    print("Arrival")
+
+    #Print third movie
+    print(Whiplash)
+
+    #Print fourth movie
+    print("Stalker")
+
+    #Print fifth movie
+    print("Pacific Rim")
+
+print("These are my top five movies")
+topfivemovies()
+git congig --global user.email "824067@mystma.org"
+git congig --global user.name "Isaac Ophoven"
