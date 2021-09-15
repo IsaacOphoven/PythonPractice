8f4be7ca1588b9ee835dc57352fe06202b4d304a
isaacophoven <824067@mystma.org>
2021-09-15 09:12:37 -0500

    now now now now

--- MKain.py
+++ MKain.py
@@ -15,6 +15,54 @@ def topFiveMovies():
     print("Pacific Rim")
 
 print("These are my top five movies")
-topfivemovies()
-git congig --global user.email "824067@mystma.org"
-git congig --global user.name "Isaac Ophoven"
+
+def topTreeGames():
+    #first game
+    print("Super Smash Bros Wii U")
+
+    #second game
+    print("The Legend of Zelda Breath of the Wild")
+
+    #Third Game
+    print("Tenkaichi 3")
+
+print("these are my top 3 games")
+
+def topTreePizzas():
+    #first pizza
+    print("Supreme Pizza")
+
+    #second pizza
+    print("three meat pizza")
+
+    #third pizza
+    print("Sausage Pizza")
+
+print("My top Three Pizzas")
+
+def threeBestNames():
+    #first name
+    print("Weegus")
+
+    #second name
+    print("Legwurd")
+
+    #third name
+    print("fleegloo")
+
+print("my top three names")
+
+def mySiblings():
+    #first sibling
+    print("Morgan")
+
+    #second sibling
+    print("Sydney")
+
+    #third sibling
+    print("Eli")
+
+    #fourth sibling
+    print("Isaiah")
+
+print("my siblings")
