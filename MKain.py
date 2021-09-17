2ee0c3bb08f0f6d1dff00da51e49c7f207f8366a
isaacophoven <824067@mystma.org>
2021-09-17 08:51:36 -0500

    waka waka waka waka

--- MKain.py
+++ MKain.py
@@ -80,14 +80,7 @@ timesTwo(1)
 
 
 def madLib(a,b,c,d,e,f,g,h,i,j):
-    print("star wars is a" + a + b)
-    print("of"+ c)
-    print("versus evil in a" + d)
-    print("far far away. There are" + e)
-    print("battles between" + f + g)
-    print("in" + h)
-    print("space and" + i)
-    print("duels with" + j)
+    print("star wars is a" + a + b + " of"+ c + " versus evil in a" + d + " far far away. There are" + e + " battles between" + f + g + " in" + h + " space and" + i + " duels with" + j)
 
-madLib("stinky",place,evil,bin,sluggish,super,garbage,stinky,epic,cats)
+madLib(" stinky"," place"," evil"," bin"," sluggish"," super"," garbage"," stinky"," epic"," cats")
 
