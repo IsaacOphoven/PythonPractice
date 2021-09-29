912fffac57cdaa7dab0ac2bdba0bbb8aca04103f
isaacophoven <824067@mystma.org>
2021-09-29 09:09:44 -0500

    awubis

--- MKain.py
+++ MKain.py
@@ -1,5 +1,7 @@
+def evenOrOdd(x):
+    if x % 2 == 0:
+        return True
+    else:
+        return False
 
-def madLib(a,b,c,d,e,f,g,h,i,j):
-    print("star wars is a" + a + b + " of"+ c + " versus evil in a" + d + " far far away. There are" + e + " battles between" + f + g + " in" + h + " space and" + i + " duels with" + j)
-
-madLib(" stinky"," place"," evil"," bin"," sluggish"," super"," garbage"," stinky"," epic"," cats")
+print(evenOrOdd(8))
